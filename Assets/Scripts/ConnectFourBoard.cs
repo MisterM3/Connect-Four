622f5da9f9e0a838cd1bdfa131349fcedbb2536a
MisterM <510249@student.saxion.nl>
2024-11-23 11:42:56 +0100

    Make Solver for ConnectFour

--- Assets/Scripts/ConnectFourBoard.cs
+++ Assets/Scripts/ConnectFourBoard.cs
@@ -5,10 +5,6 @@ using UnityEngine;
 public class ConnectFourBoard
 {
     private Player[,] _connectFourBoard;
-    public Player[,] Board
-    {
-        get => _connectFourBoard;
-    }
 
     public ConnectFourBoard(int rows, int columns)
     {
@@ -26,12 +22,57 @@ public class ConnectFourBoard
         _connectFourBoard = new Player[rows, columns];
     }
 
+    public int GetLenght(int dimension)
+    {
+        return _connectFourBoard.GetLength(dimension);
+    }
+
     public void ResetBoard()
     {
         int rows = _connectFourBoard.GetLength(0);
         int columns = _connectFourBoard.GetLength(1);
         _connectFourBoard = new Player[rows, columns];
     }
+
+    public Player GetPlayerAtLocation(Vector2Int location)
+    {
+        return GetPlayerAtLocation(location.x, location.y);
+    }
+
+    public Player GetPlayerAtLocation(int rowIndex, int columnIndex)
+    {
+        if (!IsValidPosition(rowIndex, columnIndex))
+            return Player.None;
+
+        return _connectFourBoard[rowIndex, columnIndex];
+    }
+
+    public bool IsValidPosition(Vector2Int location)
+    {
+        return IsValidPosition(location.x, location.y);
+    }
+
+    public bool IsValidPosition(int rowIndex, int columnIndex)
+    {
+
+        int boardRowCount = _connectFourBoard.GetLength(0);
+        if (rowIndex < 0 || rowIndex >= boardRowCount)
+        {
+            Debug.Log($"Row Position is outside of the board. Keep the value between 0 and {boardRowCount - 1}, current rowIndex is {rowIndex}");
+            return false;
+        }
+
+        int columnRowCount = _connectFourBoard.GetLength(1);
+        if (columnIndex < 0 || columnIndex >= columnRowCount)
+        {
+            Debug.Log($"Column Position is outside of the board. Keep the value between 0 and {columnRowCount - 1}, current columnIndex is {columnIndex}");
+            return false;
+        }
+
+        return true;
+    }
+
+
 
     public bool TryAddDisk(int rowIndex, Player player)
     {
@@ -41,8 +82,12 @@ public class ConnectFourBoard
 
     public bool TryAddDisk(int rowIndex, Player player, out int columnIndexNewDisk)
     {
+        //Makes sure index is not valid if disk is not added
+        columnIndexNewDisk = -1;
+        if (!IsValidPosition(rowIndex, 0))
+            return false;
+        
         int columns = _connectFourBoard.GetLength(1);
-
         for (int columnIndex = 0; columnIndex < columns; columnIndex++)
         {
             bool isEmptySpace = _connectFourBoard[rowIndex, columnIndex] == Player.None;
@@ -56,7 +101,6 @@ public class ConnectFourBoard
         }
 
         //If code got here, then the row is full, so disk was not added
-        columnIndexNewDisk = -1;
         return false;
     }
 
--- Assets/Scripts/ConnectFourSolver.cs
+++ Assets/Scripts/ConnectFourSolver.cs
@@ -1,6 +1,16 @@
 using UnityEngine;
 
-public abstract class ConnectFourSolver : ScriptableObject
+public abstract class ConnectFourSolver : MonoBehaviour
 {
-    abstract public bool CheckIfWin(int rowIndex, int columnIndex, Player playerToCheck);
+    protected ConnectFourBoard currentBoard;
+    protected int amountToWin = 4;
+
+    public void SetupSolver(ConnectFourBoard board, int amountToWin)
+    {
+        currentBoard = board;
+        this.amountToWin = amountToWin;
+    }
+
+    public bool IsWinningMove(Vector2Int startingPosition, Player playerToCheck) => IsWinningMove(startingPosition.x, startingPosition.y, playerToCheck);
+    abstract public bool IsWinningMove(int rowIndex, int columnIndex, Player playerToCheck);
 }
